198360c0f8f852199d23abcc80bf630866402d68
JordanAllspaw <Allspaw.j@gmail.com>
2016-07-21 14:15:17 -0400

    basic gripper tools

---
+++ scripts/calibrateLeft.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import rospy
+
+from baxter_interface import Gripper
+
+if __name__ == '__main__':
+    rospy.init_node('gripperTools', anonymous=True)
+    gripper = Gripper('left')
+    gripper.calibrate()
